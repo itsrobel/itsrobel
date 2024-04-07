e76f54a48cab9ac2d3cfb4a05058e328cbe7bf5b
robel schwarz <itsrobel.schwarz@gmail.com>
2024-04-07 18:54:23 -0400

    fixed any type

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,9 +1,9 @@
-import getReadingTime from 'reading-time';
-import { toString } from 'mdast-util-to-string';
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
 
 // https://docs.astro.build/en/recipes/reading-time/#recipe
 export function remarkReadingTime() {
-  return function (tree, { data }) {
+  return function (tree: any, { data }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text will give us minutes read as a friendly string,
