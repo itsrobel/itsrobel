3864df00d34376f1d5822e29420750a5243b9ca4
robel schwarz <itsrobel.schwarz@gmail.com>
2024-05-06 23:23:41 -0400

    removed open endpoints

--- src/pages/example/methods.ts
+++ src/pages/example/methods.ts
@@ -1,37 +1,37 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = ({ params, request }) => {
-  console.log("some shit");
-  return new Response(
-    JSON.stringify({
-      message: "This was a GET!",
-    }),
-  );
-};
-
-export const POST: APIRoute = ({ request }) => {
-  return new Response(
-    JSON.stringify({
-      message: "This was a POST!",
-    }),
-  );
-};
-
-export const DELETE: APIRoute = ({ request }) => {
-  return new Response(
-    JSON.stringify({
-      message: "This was a DELETE!",
-    }),
-  );
-};
-
-export const ALL: APIRoute = ({ request }) => {
-  return new Response(
-    JSON.stringify({
-      message: `This was a ${request.method}!`,
-    }),
-  );
-};
+// import type { APIRoute } from "astro";
+//
+// export const GET: APIRoute = ({ params, request }) => {
+//   console.log("some shit");
+//   return new Response(
+//     JSON.stringify({
+//       message: "This was a GET!",
+//     }),
+//   );
+// };
+//
+// export const POST: APIRoute = ({ request }) => {
+//   return new Response(
+//     JSON.stringify({
+//       message: "This was a POST!",
+//     }),
+//   );
+// };
+//
+// export const DELETE: APIRoute = ({ request }) => {
+//   return new Response(
+//     JSON.stringify({
+//       message: "This was a DELETE!",
+//     }),
+//   );
+// };
+//
+// export const ALL: APIRoute = ({ request }) => {
+//   return new Response(
+//     JSON.stringify({
+//       message: `This was a ${request.method}!`,
+//     }),
+//   );
+// };
 
 // let data = await response.json();
 // console.log(data);
